a2cdd2d3f49ea053ce8fcad3a1df6936c29f4d8a
Ismayil Seyidmammadli <seyidmammadli_i@itstep.edu.az>
2023-07-13 09:51:33 +0400

    Add project files.

---
+++ WpfApp16/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp16.Command;
+using WpfApp16.Views;
+
+namespace WpfApp16.ViewModels
+{
+    public class MainViewModel
+    {
+        public ICommand? NewWindow { get; set; }
+
+
+        public MainViewModel()
+        {
+            NewWindow = new RelayCommand(ExecuteNewWindow, CanExecuteNewWindow);
+        }
+
+        void ExecuteNewWindow(object? parameter)
+        {
+            AddView addview = new AddView();
+            addview.ShowDialog();
+        }
+        bool CanExecuteNewWindow(object? parameter)
+        {
+            return true;
+        }
+
+    }
+}
